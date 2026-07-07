60bbaf66a0a51c6d813ae1ec19ff2f906fc647fa
Ayushanibarik <ayushanibarik@gmail.com>
2026-07-07 21:24:55 +0530

    Switch to WebSocketSession for 100% reliable TCP connection

--- app/src/services/SessionManager.ts
+++ app/src/services/SessionManager.ts
@@ -1,15 +1,15 @@
 import { AbstractSession } from ".";
-import PeerSession from "./PeerSession.ts";
+import WebSocketSession from "./WebSocketSession.ts";
 
 export default class SessionManager {
-    static defaultImpl = 'PEERJS';
+    static defaultImpl = 'WEBSOCKET';
 
     static create({name}: {name: string}): AbstractSession {
         let instance;
 
         switch(SessionManager.defaultImpl) {
-            case 'PEERJS':
-                instance = new PeerSession();
+            case 'WEBSOCKET':
+                instance = new WebSocketSession();
                 break;
             default:
                 throw new Error(`Unknown session type '${SessionManager.defaultImpl}'`);
